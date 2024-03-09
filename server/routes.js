a3f6f4ed6f3adb0c03ee5805a3e8d2a4ea77a5be
Kristoffer West <kristoffer.c.west@gmail.com>
2024-03-09 11:25:38 +0100

    constructor...

--- server/routes.js
+++ server/routes.js
@@ -2,15 +2,13 @@ const express = require('express');
 const router = express.Router();
 const EmailService = require('./Services/EmailService');
 
-router.get('/', (req, res) => {
-    res.send('Hello from the GET route!');
-});
+const emailService = new EmailService();
 
 router.post('/sendemail', async (req, res) => {
     const data = req.body;
 
     try {
-        await EmailService.sendEmail(data);
+        await emailService.sendEmail(data);
         res.json({message: 'Success!'});
     } catch (e) {
         console.log(e);
