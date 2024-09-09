b10d042348b1e9aaaf0dc68e2b729c557564da41
Kristoffer West <kristoffer.c.west@gmail.com>
2024-09-09 18:35:35 +0200

    Fix

--- server/routes.js
+++ server/routes.js
@@ -5,6 +5,7 @@ const EmailService = require('./Services/EmailService');
 const ImageService = require('./Services/ImageService');
 
 const emailService = new EmailService();
+const imageService = new ImageService();
 const upload = multer({ dest: 'uploads/' });
 
 router.post('/sendemail', async (req, res) => {
@@ -35,7 +36,7 @@ router.post('/uploadimages', upload.array('images', 10), async (req, res) => {
     const data = req.body;
 
     try {
-        await ImageService.saveImage(req.files);
+        await imageService.saveImage(req.files);
         res.json({message: 'Success!'});
     } catch (e) {
         console.log(e);
